977873476ffe3ddef3cee00e387d5f5ca4aa43c5
Manuel Esteban Morales Zuarez <manuesteban1990@gmail.com>
2026-04-21 09:23:39 -0400

    feat: equal Sum checker

---
+++ src/EqualSumChecker.java
@@ -0,0 +1,11 @@
+public class EqualSumChecker {
+    static boolean hasEqualSum(int first, int second, int third) {
+        return (first + second) == third;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(hasEqualSum(1, 1, 1));
+        System.out.println(hasEqualSum(1, 1, 2));
+        System.out.println(hasEqualSum(1, -1, 0));
+    }
+}
